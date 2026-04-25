9b88ffb20634452c370988a2610f932194b7f45e
htcstorepj1-oss <htc.storepj1@gmail.com>
2026-04-25 11:08:02 -0300

    Delete memoria.js

--- memoria.js
+++
@@ -1,9 +0,0 @@
-function salvarMemoria(pergunta, resposta) {
-  let memoria = JSON.parse(localStorage.getItem("memoria")) || [];
-  memoria.push({ pergunta, resposta });
-  localStorage.setItem("memoria", JSON.stringify(memoria));
-}
-
-function carregarMemoria() {
-  return JSON.parse(localStorage.getItem("memoria")) || [];
-}
